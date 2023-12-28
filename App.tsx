c17d9a48992617c038c62954b88a13390d96fb29
zubair-999 <zubair-999>
2023-12-28 16:30:26 +0500

    added animated splash screen on both platforms

--- App.tsx
+++ App.tsx
@@ -1,25 +1,50 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import {StatusBar, StyleSheet} from 'react-native';
 import {useDispatch} from 'react-redux';
 import 'react-native-devsettings';
 import SplashScreen from 'react-native-splash-screen';
+import FastImage from 'react-native-fast-image';
 
+import {Types} from './src/Store/Types';
 import Navigation from './src/Navigation/index';
 import {hasCameraAndMicrophoneAllowed} from './src/Utils';
-import {Types} from './src/Store/Types';
+import {colors} from './src/Assets/Styles';
 
 function App() {
   const dispatch = useDispatch();
+  const [displayGif, setDisplayGif] = useState(true);
 
   useEffect(() => {
     SplashScreen.hide();
-    dispatch({type: Types.CLEAR_NEW_INSPECTION});
-    hasCameraAndMicrophoneAllowed().then();
+    if (displayGif) {
+      setTimeout(() => {
+        setDisplayGif(false);
+      }, 3000);
+    } else {
+      dispatch({type: Types.CLEAR_NEW_INSPECTION});
+      hasCameraAndMicrophoneAllowed().then();
+    }
     return () => {
       dispatch({type: Types.CLEAR_NEW_INSPECTION});
     };
-  }, []);
+  }, [displayGif]);
 
-  return <Navigation />;
+  return displayGif ? (
+    <>
+      <FastImage
+        source={require('./src/Assets/Images/chexDSPSlpash.gif')}
+        resizeMode={'contain'}
+        style={[StyleSheet.absoluteFill, {backgroundColor: colors.white}]}
+      />
+      <StatusBar
+        backgroundColor="transparent"
+        barStyle="light-content"
+        translucent={true}
+      />
+    </>
+  ) : (
+    <Navigation />
+  );
 }
 
 export default App;
