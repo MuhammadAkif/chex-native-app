4e2779bcadfdc270317e66bbc1c0c2387d42b4d9
zubair-999 <zubair-999>
2024-02-09 15:55:09 +0500

    fixed remarks scroll issue on android

--- src/Screens/InspectionDetailScreen.js
+++ src/Screens/InspectionDetailScreen.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import {View, StyleSheet, FlatList, Platform} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Platform,
+  Text,
+  ScrollView,
+} from 'react-native';
 
-import {NewInspectionStyles} from '../Assets/Styles';
+import {colors, NewInspectionStyles} from '../Assets/Styles';
 import {
   AndroidMediaViewModal,
   DisplayMediaModal,
   RenderInspectionDetail,
   RenderInspectionDetailHeader,
 } from '../Components';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+import {CrossFilled, Tick} from '../Assets/Icons';
 
 const InspectionDetailScreen = ({
   detailsFiles,
@@ -37,16 +49,49 @@ const InspectionDetailScreen = ({
     )}
     <View
       style={[NewInspectionStyles.bodyContainer, {paddingHorizontal: '5%'}]}>
+      <View style={styles.headerContainer}>
+        <Text style={[styles.headerText, styles.textColor]}>
+          Inspection Details
+        </Text>
+        <View style={styles.finalStatusContainer}>
+          <Text style={[styles.text, styles.textColor, {width: wp('30%')}]}>
+            Final Status
+          </Text>
+          {finalStatus && finalStatus.toLowerCase() === 'pass' ? (
+            <Tick height={hp('3%')} width={wp('8%')} color={colors.deepGreen} />
+          ) : (
+            <CrossFilled
+              height={hp('3%')}
+              width={wp('8%')}
+              color={colors.red}
+            />
+          )}
+          <Text style={[styles.text, styles.statusText, styles.textColor]}>
+            {finalStatus && finalStatus.toLowerCase() === 'pass'
+              ? 'No Damage Detected'
+              : 'Damage Detected'}
+          </Text>
+        </View>
+        <View style={styles.statusDescriptionContainer}>
+          <ScrollView>
+            <Text style={[styles.text, styles.textColor]}>
+              {remarks || 'No Remarks'}
+            </Text>
+          </ScrollView>
+        </View>
+      </View>
       <View style={styles.bodyContainer}>
         <FlatList
           data={detailsFiles}
           numColumns={2}
-          ListHeaderComponent={() => (
-            <RenderInspectionDetailHeader
-              finalStatus={finalStatus}
-              remarks={remarks}
-            />
-          )}
+          // ListHeaderComponent={() =>
+          //   Platform.OS === 'android' && (
+          //     <RenderInspectionDetailHeader
+          //       finalStatus={finalStatus}
+          //       remarks={remarks}
+          //     />
+          //   )
+          // }
           renderItem={({item}) => (
             <RenderInspectionDetail
               item={item}
@@ -63,5 +108,40 @@ const styles = StyleSheet.create({
   bodyContainer: {
     flex: 2,
   },
+  headerContainer: {
+    height: hp('30%'),
+    justifyContent: 'space-evenly',
+    borderBottomWidth: 1,
+    borderColor: '#ECECEC',
+    // paddingBottom: 20,
+  },
+  finalStatusContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: '5%',
+    borderRadius: 10,
+    backgroundColor: colors.silverGray,
+  },
+  statusDescriptionContainer: {
+    height: hp('12%'),
+    width: '100%',
+    padding: '3%',
+    borderRadius: 10,
+    backgroundColor: colors.lightGray,
+  },
+  headerText: {
+    paddingHorizontal: '2%',
+    fontSize: hp('2.5%'),
+    fontWeight: 'bold',
+  },
+  text: {
+    fontSize: hp('1.8%'),
+    // textTransform: 'capitalize',
+  },
+  statusText: {
+    fontWeight: 'bold',
+    marginLeft: '2%',
+  },
+  textColor: {color: colors.black},
 });
 export default InspectionDetailScreen;
