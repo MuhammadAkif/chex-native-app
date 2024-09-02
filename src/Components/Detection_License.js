17d13c4d6649f16c48472384bec9bede34c63a8f
Shahroz Khadim <ShahrozKhadim2020@gmail.com>
2024-09-02 11:45:24 +0500

    work in progress of license plate detection model

--- src/Components/Detection_License.js
+++ src/Components/Detection_License.js
@@ -24,11 +24,11 @@ import {useResizePlugin} from 'vision-camera-resize-plugin';
 import {BackArrow} from '../Assets/Icons';
 import {PHYSICAL_DEVICES} from '../Constants';
 import {colors, PreviewStyles} from '../Assets/Styles';
-import {CameraFooter} from './index';
+import {CameraFooter, LoadingIndicator} from './index';
 import {Models} from '../Assets/AI_Models';
-import LoadingIndicator from './LoadingIndicator';
+import {useRunOnJS} from 'react-native-worklets-core';
 
-const Object_Detection = ({navigation}) => {
+const Detection_License = ({navigation}) => {
   const isFocused = useIsFocused();
   const {resize} = useResizePlugin();
   const cameraRef = useRef();
@@ -44,16 +44,24 @@ const Object_Detection = ({navigation}) => {
 
   //AI Model initialization
   //This is working, it detects any object
-  const objectDetection = useTensorflowModel(Models.model_One);
+  // const objectDetection = useTensorflowModel(Models.model_One);
   //This model is for detecting license plate
-  // const objectDetection = useTensorflowModel(Models.model_Three);
+  const objectDetection = useTensorflowModel(Models.model_Three);
 
   //AI Model initialization status check
   const model =
     objectDetection.state === 'loaded' ? objectDetection.model : undefined;
 
+  let show = true;
+  const sayHello = useRunOnJS(output => {
+    if (show) {
+      const length = output[0];
+      console.log({length});
+      show = false;
+    }
+  }, []);
   //This processor is for license plate detecting
-  const license_frameProcessor = useFrameProcessor(
+  /*const license_frameProcessor = useFrameProcessor(
     frame => {
       'worklet';
       if (model == null) {
@@ -69,145 +77,76 @@ const Object_Detection = ({navigation}) => {
           pixelFormat: 'rgb',
           dataType: 'float32',
         });
+        console.log(Array.isArray(resized));
 
         // 2. Run the model with the properly formatted input buffer synchronously
         const outputs = model?.runSync([resized]);
-        console.log('length:: ', outputs[0]);
 
-        let detection_boxes = outputs[0];
+        if (outputs?.length > 0) {
+          const outputTensor = outputs[0];
 
-        // Convert detection_boxes to an array if it's an object
-        if (detection_boxes && typeof detection_boxes === 'object') {
-          detection_boxes = Object.values(detection_boxes);
-        }
+          // Convert the output to a format where we can find the max value
+          const outputEntries = Object.entries(outputTensor);
+          if (outputEntries?.length > 0) {
+            // Find the entry with the highest probability
+            const [maxIndex, maxProbability] = outputEntries?.reduce(
+              (maxEntry, currentEntry) =>
+                currentEntry[1] > maxEntry[1] ? currentEntry : maxEntry,
+            );
 
-        if (!Array.isArray(detection_boxes)) {
-          console.error(
-            'detection_boxes is not properly formatted:',
-            detection_boxes,
-          );
-          return;
-        }
-
-        // console.log('Parsed detection_boxes:', detection_boxes.slice(0, 20));
-
-        // Interpret parsed detection_boxes
-        const numElementsPerDetection = 5; // Adjust if necessary: x, y, w, h, confidence
-
-        // const numDetections = detection_boxes.length / numElementsPerDetection;
-        // console.log(`Detected ${numDetections} potential objects.`);
-        const confidence = Object.values(outputs[0]);
-        // Parse detection boxes and confidence scores
-        for (
-          let i = 0;
-          i < detection_boxes.length;
-          i += numElementsPerDetection
-        ) {
-          // const [left, top, right, bottom, confidence] = detection_boxes.slice(
-          //   i,
-          //   i + numElementsPerDetection,
-          // );
-
-          if (confidence > 0.7) {
-            console.log('Matches!!!');
+            console.log(`Predicted class index: ${maxIndex}`);
+            console.log(`Confidence: ${maxProbability}`);
           }
+        } else {
+          console.log('No outputs returned from the model.');
         }
       } catch (error) {
         console.error('Frame Processor Error:', error);
       }
     },
     [model],
-  );
-
-  //This frame processor is working to detect any object model
-  const object_frameProcessor_Working = useFrameProcessor(
+  );*/
+  const license_frameProcessor = useFrameProcessor(
     frame => {
       'worklet';
       if (model == null) {
+        console.error('Model is not loaded.');
         return;
       }
+      // Resize the frame to 640x640 and convert to float32
+      // 1. Resize 4k Frame to 192x192x3 using vision-camera-resize-plugin
       const resized = resize(frame, {
         scale: {
-          width: 192,
-          height: 192,
+          width: 190,
+          height: 190,
         },
         pixelFormat: 'rgb',
-        dataType: 'uint8',
-      });
-
-      const outputs = model.runSync([resized]);
-
-      // 3. Interpret outputs accordingly
-      const detection_boxes = outputs[0];
-      const detection_classes = outputs[1];
-      const detection_scores = outputs[2];
-      const num_detections = outputs[3];
-      console.log(`Detected ${num_detections[0]} objects!`);
-
-      for (let i = 0; i < detection_boxes.length; i += 4) {
-        const confidence = detection_scores[i / 4];
-        if (confidence > 0.7) {
-          console.log('Matched!!!');
-          const left = detection_boxes[i];
-          const top = detection_boxes[i + 1];
-          const right = detection_boxes[i + 2];
-          const bottom = detection_boxes[i + 3];
-        }
-      }
-    },
-    [model],
-  );
-  /*const frameProcessor = useFrameProcessor(
-    frame => {
-      'worklet';
-      if (model == null) {
-        return;
-      }
-
-      const resized = resize(frame, {
-        scale: {
-          width: 192,
-          height: 192,
-        },
-        pixelFormat: 'rgb',
-        dataType: 'uint8',
+        dataType: 'float32',
       });
 
       // 2. Run model with given input buffer synchronously
       const outputs = model.runSync([resized]);
-      const {length} = outputs;
-      console.log('length: ', outputs[2]);
+      sayHello(outputs).then();
+      /* // 3. Interpret outputs accordingly
+        const detection_boxes = outputs[0];
+        const detection_classes = outputs[1];
+        const detection_scores = outputs[2];
+        const num_detections = outputs[3];
+        console.log(`Detected ${num_detections[0]} objects!`);
 
-      // 3. Interpret outputs accordingly
-      const detection_boxes = outputs[0];
-      const detection_scores = outputs[2];
-      const num_detections = outputs[3];
-      // console.log(`Detected ${num_detections[0]} objects!`);
-
-      // Prepare an array to hold detected rectangles
-      const rects = [];
-
-      for (let i = 0; i < detection_boxes.length; i += 4) {
-        const confidence = detection_scores[i / 4];
-        if (confidence > 0.7) {
-          // Extract the box coordinates
-          const left = detection_boxes[i];
-          const top = detection_boxes[i + 1];
-          const right = detection_boxes[i + 2];
-          const bottom = detection_boxes[i + 3];
-
-          // Push the rect coordinates to the array
-          // rects.push({left, top, width: right - left, height: bottom - top});
-          console.log('I am matched !!!');
-        }
-      }
-
-      // Update the detected rectangles state
-      // runOnJS(() => handleUpdate(rects));
+        for (let i = 0; i < detection_boxes.length; i += 4) {
+          const confidence = detection_scores[i / 4];
+          if (confidence > 0.7) {
+            // 4. Draw a red box around the detected object!
+            const left = detection_boxes[i];
+            const top = detection_boxes[i + 1];
+            const right = detection_boxes[i + 2];
+            const bottom = detection_boxes[i + 3];
+          }
+        }*/
     },
     [model],
-  );*/
-
+  );
   return (
     <View style={PreviewStyles.container}>
       <LoadingIndicator isLoading={objectDetection.state === 'loading'} />
@@ -217,7 +156,7 @@ const Object_Detection = ({navigation}) => {
             ref={cameraRef}
             style={StyleSheet.absoluteFill}
             device={device}
-            frameProcessor={object_frameProcessor_Working}
+            frameProcessor={license_frameProcessor}
             photo={true}
             audio={false}
             enableDepthData={true}
@@ -244,4 +183,4 @@ const Object_Detection = ({navigation}) => {
     </View>
   );
 };
-export default Object_Detection;
+export default Detection_License;
