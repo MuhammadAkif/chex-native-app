e55c0aa6cf3e22f036164c2a75f10bf9b62ab2bb
Shahroz Khadim <ShahrozKhadim2020@gmail.com>
2024-11-26 18:25:08 +0500

    react-native-fs and react-native-opencv3 integration phase

--- android/app/src/main/java/com/chex_ai/MainApplication.java
+++ android/app/src/main/java/com/chex_ai/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.rnfs.RNFSPackage; // <------- add package
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new RNFSPackage());
           return packages;
         }
 
