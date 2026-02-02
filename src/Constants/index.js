d2ff9233ac744bd6181b31a82c2ab4bceeca0d47
Ali <alihassan.sync@gmail.com>
2026-02-02 19:07:18 +0500

    smartlook key move from env to constant

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,5 +1,5 @@
-import {Platform} from 'react-native';
-import {IMAGES} from '../Assets/Images';
+import { Platform } from 'react-native';
+import { IMAGES } from '../Assets/Images';
 
 // Vehicle Types Constants
 export const VEHICLE_TYPES = {
@@ -30,14 +30,14 @@ const ENV_TYPE_URL = {
 
 const version = '1';
 export const API_VERSION_PATH = '/api/v' + version + '/';
-export const API_BASE_URL = ENV_TYPE_URL.staging;
+export const API_BASE_URL = ENV_TYPE_URL.production;
 export const generateApiUrl = path => API_BASE_URL + API_VERSION_PATH + path;
 export const S3_BUCKET_BASEURL = process.env.S3_BUCKET_BASEURL;
 export const EXTRACT_NUMBER_PLATE_WITH_AI = process.env.EXTRACT_NUMBER_PLATE_URL;
 export const nightImageCheckAI = process.env.NIGHT_IMAGE_CHECK;
 export const MILEAGE_EXTRACTION = process.env.MILEAGE_EXTRACT;
 export const AI_API_TOKEN = process.env.AI_API_TOKEN;
-export const SMARTLOOK_PROJECT_ID = process.env.SMARTLOOK_PROJECT_ID;
+export const SMARTLOOK_PROJECT_ID = '77967539d73859f97a4f9cc54d00dac3b6f00deb';
 
 // API endpoints
 export const API_ENDPOINTS = {
